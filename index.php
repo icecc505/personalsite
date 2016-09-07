ade5495b67f12803e2bf50bf7777451cd257df7b
icecc505 <releasedice@gmail.com>
2016-09-07 06:48:17 -0600

    added many re done files
    re adding my almost finished website

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html >
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Design Plebs</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+  <body>
+
+    <html>
+
+  <head>
+
+    <title>Design The Pleb Way</title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-smooth-scroll/1.5.5/jquery.smooth-scroll.min.js"></script>
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"
+    integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+  </head>
+  <body>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300'
+    rel='stylesheet' type='text/css'>
+    <div class="navbar">
+      <ul class="navbar-container">
+        <li><a href="#" class="left-underline nav-button brand-logo">DP</a></li>
+        <li class="nav-item"><a href="#section-3" class="left-underline nav-button" data-scroll>Contact</a></li>
+        <li class="nav-item"><a href="#section-2" class="left-underline nav-button" data-scroll>About</a></li>
+        <li class="nav-item active"><a href="#section-1" class="left-underline nav-button" data-scroll>Home</a></li>
+      </ul>
+    </div>
+    <div class="parallax p1" id="section-1">
+      <hgroup>
+        <h1>DESIGN</h1>
+        <h2>THE PLEB WAY</h2>
+      </hgroup>
+    </div>
+
+    <div class="parallax p2" id="section-2">
+      <hgroup>
+        <h1>Well...</h1>
+        <h2>Im almost exciting</h2>
+      </hgroup>
+    </div>
+    <div class="row">
+      <div class="col-3">
+        <h1>First Section Col1</h1>
+        <p>Hey there my name is Timothy im a very new coder and just all around geeky sort of guy
+          looking to hone my skills and make some awesome stuff that people may or may not like
+          I do coding, photoshops, colorings and some digital drawings/design
+          below is my contact info message me with specifics of what you'd like
+          done or if you would you'd like to just see and or talk about anything i do!</p>
+      </div>
+    <div class="parallax p3">
+      <hgroup>
+        <h1>Well</h1>
+        <h2>This is some bold info!</h2>
+      </hgroup>
+    </div>
+    <footer>
+      <div class="row" id="section-3">
+        <div class="col-3">
+          <p>Below is some of my contact info request something or
+            stalk me on my social media sites its all good :)</p>
+            <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+            <a href="https://instagram.com/icedpleb" target="_blank"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+            <a href="https://twitter.com/soullesswolf505" target="_blank"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
+
+
+
+        </div>
+      </div>
+
+    </footer>
+
+    <div class = "container" id="John">
+
+  </body>
+</html>
+<script src='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css'></script>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="js/index.js"></script>
+      </body>
+
+</html>
